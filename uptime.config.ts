9d1f571206aa6b5c8696ef118000b4540fdd23b1
yuanguangshan <yuanguangshan@gmail.com>
2024-08-11 12:50:21 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -1,11 +1,11 @@
 const pageConfig = {
   // Title for your status page
-  title: "lyc8503's Status Page",
+  title: "github's Status Page",
   // Links shown at the header of your status page, could set `highlight` to `true`
   links: [
-    { link: 'https://github.com/lyc8503', label: 'GitHub' },
-    { link: 'https://blog.lyc8503.net/', label: 'Blog' },
-    { link: 'mailto:me@lyc8503.net', label: 'Email Me', highlight: true },
+    { link: 'https://github.com/yuanguangshan', label: 'GitHub' },
+    { link: 'https://wiki.want.biz', label: 'wiki' },
+    { link: 'https://www.want.biz', label: 'Nas', highlight: true },
   ],
 }
 
